ae2d48982411072ca5d5757d9b8b19327b697205
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-09-15 09:37:33 -0400

    Credit card

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //variable declarations
+        int balance = 5000;
+        double MonthOne = 1.17;
+        double monthTwo = 1.3689;
+
+        System.out.println("After one month, the balance of a $5000 credit card is $" + 5000 * 1.17 + ".");
+        System.out.println("After two months, the balance of a $5000 credit card is $" + 5000 * 1.3689 + ".");
+
+
+
     }
 }
